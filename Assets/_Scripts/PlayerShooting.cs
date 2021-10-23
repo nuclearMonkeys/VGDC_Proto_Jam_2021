c688b1f4cc76ee434d4bc8ff8771e9ae13099a41
nuclearMoneky <lyndond@uci.edu>
2021-10-23 16:11:03 -0700

    ADDED COMBO SYSTEM

--- Assets/_Scripts/PlayerShooting.cs
+++ Assets/_Scripts/PlayerShooting.cs
@@ -10,25 +10,33 @@ public class PlayerShooting : MonoBehaviour
     public BaseBullet bulletPrefab;
     public float slashComboLimit = 1;
     public float fireRate = 0.7f;
-    public float slashRate = 0.7f;
+    public float slashComboCooldown = 0.3f; // This is the cool down for the sword combo
+    public float slashRate = 0.7f;          // This is the the time between different sets of combos
     private Vector2 mousePosition;
     private Vector2 firingOrigin;
     
     private Rigidbody2D body;
     private RaycastHit2D hit;
+    private int currentCombo;
     private bool canFire = true;
     private bool canSlash = true;
+
+    float lastTime;
 
     // private enemyBehavior damaging;
     // private weaponInfo useClip;
 
-    // Update is called once per frame
+    private void Start() 
+    {
+        StartCoroutine(slashEnumerator());
+    }
+
     private void Update() 
     {
         if (Input.GetAxisRaw("Fire1") != 0) 
             Fire();
-        if (Input.GetAxisRaw("Fire2") != 0)
-            Slash();
+        // if (Input.GetAxisRaw("Fire2") != 0)
+        //     Slash();
     }
 
     public void Fire() 
@@ -37,11 +45,11 @@ public class PlayerShooting : MonoBehaviour
             StartCoroutine(fireEnumerator());
     }
 
-    public void Slash() 
-    {
-        if (canSlash)
-            StartCoroutine(slashEnumerator());
-    }
+    // public void Slash() 
+    // {
+    //     if (canSlash)
+    //         StartCoroutine(slashEnumerator());
+    // }
 
     IEnumerator fireEnumerator() 
     {
@@ -54,10 +62,37 @@ public class PlayerShooting : MonoBehaviour
 
     IEnumerator slashEnumerator() 
     {
-        playerSword.SetActive(true);
-        canSlash = false;
-        yield return new WaitForSeconds(slashRate);
-        canSlash = true;
-        playerSword.SetActive(false);
+        while (true) 
+        {
+            if (Input.GetButtonDown("Fire2")) 
+            {
+                playerSword.SetActive(true);
+                currentCombo++;
+                Debug.Log("Attack" + currentCombo);
+                lastTime = Time.time;
+                yield return new WaitForSeconds(slashRate);
+                playerSword.SetActive(false);
+
+                while(currentCombo < slashComboLimit && (Time.time - lastTime) < slashComboCooldown) 
+                {
+                    if (Input.GetButtonDown("Fire2"))  
+                    {
+                        playerSword.SetActive(true);
+                        currentCombo++;
+                        Debug.Log("Attack" + currentCombo);
+                        lastTime = Time.time;
+                        yield return new WaitForSeconds(slashRate);
+                        playerSword.SetActive(false);
+                    }
+                    yield return null;
+                }
+
+                currentCombo = 0;
+                playerSword.SetActive(false);
+                yield return new WaitForSeconds(slashComboCooldown - (Time.time - lastTime));
+            }
+            yield return null;
+            playerSword.SetActive(false);
+        }
     }
 }
