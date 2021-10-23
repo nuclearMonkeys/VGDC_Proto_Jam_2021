5c78112e182a4ad5b2b88fdc3d74e83d7beb6718
nuclearMoneky <lyndond@uci.edu>
2021-10-23 13:46:06 -0700

    Added slash

--- Assets/_Scripts/PlayerShooting.cs
+++ Assets/_Scripts/PlayerShooting.cs
@@ -6,6 +6,7 @@ public class PlayerShooting : MonoBehaviour
 {
     [Header("Shooting Number Variables")]
     public Transform firingPoint;
+    public GameObject playerSword;
     public BaseBullet bulletPrefab;
     private Vector2 mousePosition;
     private Vector2 firingOrigin;
@@ -22,6 +23,8 @@ public class PlayerShooting : MonoBehaviour
     {
         if (Input.GetAxisRaw("Fire1") != 0) 
             Fire();
+        if (Input.GetAxisRaw("Fire2") != 0)
+            Slash();
     }
 
     public void Fire() 
@@ -30,6 +33,21 @@ public class PlayerShooting : MonoBehaviour
         {
             StartCoroutine(fireEnumerator());
         }
+    }
+
+    public void Slash() 
+    {
+        if (canFire)
+            StartCoroutine(slashEnumerator());
+    }
+
+    IEnumerator slashEnumerator() 
+    {
+        playerSword.SetActive(true);
+        canFire = false;
+        yield return new WaitForSeconds(0.5f);
+        canFire = true;
+        playerSword.SetActive(false);
     }
 
     IEnumerator fireEnumerator() 
